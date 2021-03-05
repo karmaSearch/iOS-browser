b52fecb8afd98a42085a7d3bf5a701c674dfef6f
Jörn Ehmann <60604005+ecotopian@users.noreply.github.com>
2021-03-05 10:24:35 +0100

    Try skip 2fa (#12)
    * try different apple id
    * remove code sign failing script
    * Count up bundle
    * remove one pw leftover

--- carthage_bootstrap_moz_services.sh
+++ carthage_bootstrap_moz_services.sh
@@ -19,3 +19,6 @@ carthage bootstrap application-services --platform ios --color auto --cache-buil
 # fi
 
 
+# Move Glean script to source folder from MozillaAppServices.framework
+# as we don't want to ship our app with this Glean script inside framework
+mv Carthage/Build/iOS/MozillaAppServices.framework/sdk_generator.sh ./
