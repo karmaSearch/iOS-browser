08febf27964c7778d067367739c93697dea965d8
vauxhall <zero.griffin@gmail.com>
2021-03-18 09:39:33 +0100

    Make markets controller a themed table view controller (#23)

--- Client/Ecosia/UI/MarketsController.swift
+++ Client/Ecosia/UI/MarketsController.swift
@@ -17,58 +17,43 @@ final class Markets {
     }
 }
 
-final class MarketsController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    private weak var table: UITableView!
+final class MarketsController: ThemedTableViewController {
     private let identifier = "market"
 
-    required init?(coder: NSCoder) { nil }
-    init() {
-        super.init(nibName: nil, bundle: nil)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.theme.ecosia.primaryBackground
         navigationItem.title = .localized(.searchRegion)
-
-        let table = UITableView()
-        table.translatesAutoresizingMaskIntoConstraints = false
-        table.tintColor = UIColor.theme.ecosia.primaryBrand
-        table.delegate = self
-        table.dataSource = self
-        table.backgroundColor = .clear
-        view.addSubview(table)
-        self.table = table
-        
-        table.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        table.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        table.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        table.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        applyTheme()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         Markets.all.firstIndex { User.shared.marketCode == $0.id }.map {
-            table.scrollToRow(at: .init(row: $0, section: 0), at: .middle, animated: true)
+            tableView.scrollToRow(at: .init(row: $0, section: 0), at: .middle, animated: true)
         }
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_: UITableView, numberOfRowsInSection: Int) -> Int {
         Markets.all.count
     }
 
-    func tableView(_ : UITableView, cellForRowAt: IndexPath) -> UITableViewCell {
-        let cell = table.dequeueReusableCell(withIdentifier: identifier) ?? ThemedTableViewCell(style: .default, reuseIdentifier: identifier)
+    override func tableView(_ tableView: UITableView, cellForRowAt: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier) ?? ThemedTableViewCell(style: .default, reuseIdentifier: identifier)
         cell.textLabel!.text = Markets.all[cellForRowAt.row].name
         cell.textLabel!.textColor = UIColor.theme.tableView.rowText
         cell.accessoryType = User.shared.marketCode == Markets.all[cellForRowAt.row].id ? .checkmark : .none
         return cell
     }
     
-    func tableView(_: UITableView, didSelectRowAt: IndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt: IndexPath) {
         guard Markets.all[didSelectRowAt.row].id != User.shared.marketCode else { return }
         User.shared.marketCode = Markets.all[didSelectRowAt.row].id
-        table.reloadData()
+        tableView.reloadData()
         Analytics.shared.market(User.shared.marketCode.rawValue)
         Goodall.shared.refresh()
     }
