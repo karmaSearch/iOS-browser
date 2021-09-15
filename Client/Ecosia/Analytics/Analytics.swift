9d12ff1e6579f387cadb2927d24841d8873fa7b0
vauxhall <zero.griffin@gmail.com>
2021-09-15 12:11:56 +0200

    Update snowplow (#148)
    * Update package version
    
    * Update tracker controller
    
    * Update install
    
    * Update events
    
    * Update events
    
    * Update events
    
    * Update events

--- Client/Ecosia/Analytics/Analytics.swift
+++ Client/Ecosia/Analytics/Analytics.swift
@@ -3,156 +3,138 @@ import SnowplowTracker
 import Core
 
 final class Analytics {
+    private static var tracker: TrackerController {
+        Snowplow
+            .createTracker(namespace: "ios_sp",
+                           network: .init(endpoint: Environment.current.snowplow),
+                           configurations: [TrackerConfiguration()
+                                                .appId(Bundle.version)
+                                                .platformContext(true)
+                                                .geoLocationContext(true),
+                                            SubjectConfiguration()
+                                                .userId(User.shared.analyticsId.uuidString)])
+    }
+    
     static let shared = Analytics()
-    private let tracker: SPTracker
+    private(set) var tracker: TrackerController
 
     private init() {
-        tracker = .build {
-            $0?.setAppId(Bundle.version)
-            $0?.setTrackerNamespace("ios_sp")
-            
-            $0?.setEmitter(.build {
-                $0?.setUrlEndpoint(Environment.current.snowplow)
-            })
-            
-            let subject = SPSubject(platformContext: true, andGeoContext: true)
-            subject?.setUserId(User.shared.analyticsId.uuidString)
-            $0?.setSubject(subject)
-        }
+        tracker = Self.tracker
     }
     
     func install() {
-        SPSelfDescribingJson(schema: "iglu:org.ecosia/ios_install_event/jsonschema/1-0-0", andData: ["app_v": Bundle.version] as NSObject).map { data in
-            tracker.track(SPUnstructured.build {
-                $0.setEventData(data)
-            })
-        }
-    }
-    
-    func activity(_ action: Action.Activity) {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.activity.rawValue)
-            $0.setAction(action.rawValue)
-            $0.setLabel("inapp")
-        })
-    }
-
-    func browser(_ action: Action.Browser, label: Label.Browser, property: Property? = nil) {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.browser.rawValue)
-            $0.setAction(action.rawValue)
-            $0.setLabel(label.rawValue)
-            $0.setProperty(property?.rawValue)
-        })
-    }
-
-    func navigation(_ action: Action, label: Label.Navigation) {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.navigation.rawValue)
-            $0.setAction(action.rawValue)
-            $0.setLabel(label.rawValue)
-        })
-    }
-
-    func navigationOpenNews(_ id: String) {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.navigation.rawValue)
-            $0.setAction(Action.open.rawValue)
-            $0.setLabel(Label.Navigation.news.rawValue)
-            $0.setProperty(id)
-        })
-    }
-    
-    func navigationChangeMarket(_ new: String) {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.navigation.rawValue)
-            $0.setAction("change")
-            $0.setLabel("market")
-            $0.setProperty(new)
-        })
-    }
-
-    func deeplink() {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.external.rawValue)
-            $0.setAction(Action.receive.rawValue)
-            $0.setLabel("deeplink")
-        })
-    }
-    
-    func defaultBrowser() {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.external.rawValue)
-            $0.setAction(Action.receive.rawValue)
-            $0.setLabel("default_browser_deeplink")
-        })
+        tracker
+            .track(SelfDescribing(schema: "iglu:com.snowplowanalytics.snowplow/link_click/jsonschema/1-0-1",
+                                  payload: ["app_v": Bundle.version as NSObject]))
     }
     
     func reset() {
         User.shared.analyticsId = .init()
-        
-        guard let subject = SPSubject(platformContext: true, andGeoContext: true) else { return }
-        subject.setUserId(User.shared.analyticsId.uuidString)
-        tracker.setSubject(subject)
+        tracker = Self.tracker
+    }
+    
+    func activity(_ action: Action.Activity) {
+        tracker
+            .track(Structured(category: Category.activity.rawValue,
+                              action: action.rawValue)
+                    .label("inapp"))
+    }
+
+    func browser(_ action: Action.Browser, label: Label.Browser, property: Property? = nil) {
+        tracker
+            .track(Structured(category: Category.browser.rawValue,
+                              action: action.rawValue)
+                    .label(label.rawValue)
+                    .property(property?.rawValue))
+    }
+
+    func navigation(_ action: Action, label: Label.Navigation) {
+        tracker
+            .track(Structured(category: Category.navigation.rawValue,
+                              action: action.rawValue)
+                    .label(label.rawValue))
+    }
+
+    func navigationOpenNews(_ id: String) {
+        tracker
+            .track(Structured(category: Category.navigation.rawValue,
+                              action: Action.open.rawValue)
+                    .label(Label.Navigation.news.rawValue)
+                    .property(id))
+    }
+    
+    func navigationChangeMarket(_ new: String) {
+        tracker
+            .track(Structured(category: Category.navigation.rawValue,
+                              action: "change")
+                    .label("market")
+                    .property(new))
+    }
+
+    func deeplink() {
+        tracker
+            .track(Structured(category: Category.external.rawValue,
+                              action: Action.receive.rawValue)
+                    .label("deeplink"))
+    }
+    
+    func defaultBrowser() {
+        tracker
+            .track(Structured(category: Category.external.rawValue,
+                              action: Action.receive.rawValue)
+                    .label("default_browser_deeplink"))
     }
     
     func defaultBrowser(_ action: Action.Promo) {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.browser.rawValue)
-            $0.setAction(action.rawValue)
-            $0.setLabel("default_browser_promo")
-            $0.setProperty("home")
-        })
+        tracker
+            .track(Structured(category: Category.browser.rawValue,
+                              action: action.rawValue)
+                    .label("default_browser_promo")
+                    .property("home"))
     }
 
     func defaultBrowserSettings() {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.browser.rawValue)
-            $0.setAction(Action.open.rawValue)
-            $0.setLabel("default_browser_settings")
-        })
+        tracker
+            .track(Structured(category: Category.browser.rawValue,
+                              action: Action.open.rawValue)
+                    .label("default_browser_settings"))
     }
 
     func migration(_ success: Bool) {
-        tracker.track(SPStructured.build({
-            $0.setCategory(Category.migration.rawValue)
-            $0.setAction(success ? Action.success.rawValue : Action.error.rawValue)
-        }))
+        tracker
+            .track(Structured(category: Category.migration.rawValue,
+                              action: success ? Action.success.rawValue : Action.error.rawValue))
     }
 
     func migrationError(in migration: Migration, message: String) {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.migration.rawValue)
-            $0.setAction(Action.error.rawValue)
-            $0.setLabel(migration.rawValue)
-            $0.setProperty(message)
-        })
+        tracker
+            .track(Structured(category: Category.migration.rawValue,
+                              action: Action.error.rawValue)
+                    .label(migration.rawValue)
+                    .property(message))
     }
 
     func migrationRetryHistory(_ success: Bool) {
-        tracker.track(SPStructured.build({
-            $0.setCategory(Category.migration.rawValue)
-            $0.setAction(Action.retry.rawValue)
-            $0.setLabel(Migration.history.rawValue)
-            $0.setProperty(success ? Action.success.rawValue : Action.error.rawValue)
-        }))
+        tracker
+            .track(Structured(category: Category.migration.rawValue,
+                              action: Action.retry.rawValue)
+                    .label(Migration.history.rawValue)
+                    .property(success ? Action.success.rawValue : Action.error.rawValue))
     }
     
     func migrated(_ migration: Migration, in seconds: TimeInterval) {
-        tracker.track(SPStructured.build({
-            $0.setCategory(Category.migration.rawValue)
-            $0.setAction(Action.completed.rawValue)
-            $0.setLabel(migration.rawValue)
-            $0.setValue(seconds * 1000)
-        }))
+        tracker
+            .track(Structured(category: Category.migration.rawValue,
+                              action: Action.completed.rawValue)
+                    .label(migration.rawValue)
+                    .value(.init(value: seconds * 1000)))
     }
     
     func open(topSite: Property.TopSite) {
-        tracker.track(SPStructured.build {
-            $0.setCategory(Category.browser.rawValue)
-            $0.setAction(Action.open.rawValue)
-            $0.setLabel("top_sites")
-            $0.setProperty(topSite.rawValue)
-        })
+        tracker
+            .track(Structured(category: Category.browser.rawValue,
+                              action: Action.open.rawValue)
+                    .label("top_sites")
+                    .property(topSite.rawValue))
     }
 }
